1b3055a83a39da28d29dd9bd8b32c25b86552f05
Mark Mankarious <mz.mankarious@gmail.com>
2023-03-16 23:01:44 +0000

    adds husky script

---
+++ scripts/post_commit.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+
+if [ "$version" != "" ]; then
+    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    echo "Created a new tag, v$version"
+fi
