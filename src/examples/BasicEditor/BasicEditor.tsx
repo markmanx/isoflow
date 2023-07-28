bdd150c8dd061810b77875ebe7d74dd11bc9d3c9
Mark Mankarious <mz.mankarious@gmail.com>
2023-07-28 15:57:46 +0100

    Sets height on examples

--- src/examples/BasicEditor/BasicEditor.tsx
+++ src/examples/BasicEditor/BasicEditor.tsx
@@ -11,6 +11,7 @@ export const BasicEditor = () => {
         groups: [],
         nodes: []
       }}
+      height="100%"
     />
   );
 };
--- src/examples/CustomNode/CustomNode.tsx
+++ src/examples/CustomNode/CustomNode.tsx
@@ -21,6 +21,7 @@ export const CustomNode = () => {
           }
         ]
       }}
+      height="100%"
     />
   );
 };
