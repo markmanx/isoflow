b2416f3e984e1e474b7ef561d82259216ea3bf7b
Mark Mankarious <mz.mankarious@gmail.com>
2023-08-08 16:35:06 +0100

    fix: resolves paths after typese are compiled

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,53 +1,53 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  devtool: "eval-cheap-source-map",
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'eval-cheap-source-map',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build')
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "build"),
+      directory: path.join(__dirname, 'build')
     },
     allowedHosts: [
       '.csb.app', // So Codesandbox.io can run the dev server
       '.ngrok-free.app'
     ],
-    port: 3000,
+    port: 3000
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.svg$/,
         use: [
           {
             loader: 'svg-url-loader'
-          },
-        ],
-      },
-    ],
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, "../src/index.html"),
-    }),
-  ],
+      template: path.resolve(__dirname, '../src/index.html')
+    })
+  ]
 };
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,51 +1,51 @@
-const path = require("path");
+const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/Isoflow.tsx",
+  mode: 'production',
+  entry: './src/Isoflow.tsx',
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "index.js",
-    libraryTarget: "commonjs2",
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
   },
   externals: {
     react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React",
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
     },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM",
-    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.svg$/,
         use: [
           {
             loader: 'svg-url-loader'
-          },
-        ],
-      },
-    ],
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
     plugins: [new TsconfigPathsPlugin()]
-  },
+  }
 };
