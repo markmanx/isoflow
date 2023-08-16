6bf17402b345b62e9021ed163b2150ca1337097a
Mark Mankarious <mz.mankarious@gmail.com>
2023-08-16 16:17:29 +0100

    fix: reinstates debug tools

--- src/components/DebugUtils/DebugUtils.tsx
+++ src/components/DebugUtils/DebugUtils.tsx
@@ -23,6 +23,8 @@ export const DebugUtils = () => {
         position: 'absolute',
         top: 0,
         left: 0,
+        width: '100%',
+        height: '100%',
         pointerEvents: 'none'
       }}
     >
