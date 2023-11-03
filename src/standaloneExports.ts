d5084e28ea2a5abf02d019d21621a7ec6824bf91
Mark Mankarious <mz.mankarious@gmail.com>
2023-11-03 19:48:08 +0000

    feat: adds to standaloneExports

--- src/standaloneExports.ts
+++ src/standaloneExports.ts
@@ -2,7 +2,7 @@
 // bundle requires `window` to be present and so can't be imported into a Node environment.
 export const version = PACKAGE_VERSION;
 export * as reducers from 'src/stores/reducers';
-export { INITIAL_DATA } from 'src/config';
+export { INITIAL_DATA, INITIAL_SCENE_STATE } from 'src/config';
 export { modelSchema } from 'src/schemas/model';
 export type { IsoflowProps, InitialData } from 'src/types';
 export type * from 'src/types/model';
