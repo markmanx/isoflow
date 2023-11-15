2bbc2531c6c4b6fb46113ac18985858a4efd0a8f
Mark Mankarious <mz.mankarious@gmail.com>
2023-11-15 11:02:15 +0000

    chore: exports all validation schemas

--- src/standaloneExports.ts
+++ src/standaloneExports.ts
@@ -3,6 +3,6 @@
 export const version = PACKAGE_VERSION;
 export * as reducers from 'src/stores/reducers';
 export { INITIAL_DATA, INITIAL_SCENE_STATE } from 'src/config';
-export { modelSchema } from 'src/schemas/model';
+export * from 'src/schemas/model';
 export type { IsoflowProps, InitialData } from 'src/types';
 export type * from 'src/types/model';
